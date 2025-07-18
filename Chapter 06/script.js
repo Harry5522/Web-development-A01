e1942904690980497d394365ec852798d0e62a70
Abdullah Alali <136125725+Harry5522@users.noreply.github.com>
2025-07-18 11:24:18 +0400

    .
    .

--- Chapter 06/script.js
+++
@@ -1,8 +0,0 @@
-
-function calculateTotal() {
-  const price = parseFloat(document.getElementById("price").value);
-  const liters = parseFloat(document.getElementById("liters").value);
-  const total = price * liters;
-
-  document.getElementById("total").textContent = `Total: AED ${total.toFixed(2)}`;
-}
